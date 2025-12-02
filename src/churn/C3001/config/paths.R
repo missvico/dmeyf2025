77c4b02c93c61adc831eb1c66fcf63f742265dff
missvico <vickydiliscia@gmail.com>
2025-12-02 19:19:32 +0000

    setup C3001

---
+++ src/churn/C3001/config/paths.R
@@ -0,0 +1,32 @@
+paths <- list()
+
+# Paths Base
+paths$experimento <- "APO3001-202106"
+paths$scriptfolder <- "C3001"
+paths$user <- "vickydiliscia"
+paths$base <- paste0("/home/", paths$user, "/dmeyf2025")
+paths$bucket <- paste0("/home/", paths$user, "/buckets/b1")
+paths$experiments   <- paste0(paths$bucket, "/exp")
+
+paths$churn     <- paste0(paths$base, "/src/churn")
+paths$script <- paste0(paths$churn, "/", paths$scriptfolder)
+
+# Paths Input
+paths$datasets   <- paste0(paths$bucket, "/datasets")
+paths$datasetcrudo2   <- paste0(paths$datasets , "/competencia_02_crudo.csv.gz")
+paths$datasetcrudo3    <- paste0(paths$datasets , "/competencia_03_crudo.csv.gz")
+paths$datasetclases <- paste0(paths$datasets , "/competencia_02.csv.gz")
+
+# Paths codigos
+paths$churn     <- paste0(paths$base, "/src/churn")
+paths$functions <- paste0(paths$churn, "/functions")
+paths$script <- paste0(paths$churn, "/", paths$scriptfolder)
+
+# Resultados
+paths$outputs    <- paste0(paths$bucket, "/outputs")
+paths$results     <- paste0(paths$experiments, "/", paths$experimento )
+paths$optimization <- paste0(paths$results, "/optimization")
+paths$optimization <- paste0(paths$results , "/models")
+paths$prediction <- paste0(paths$results , "/prediction")
+paths$kaggle <- paste0(paths$results , "/kaggle")
+paths$backtesting <- paste0(paths$results , "/backtesting")
