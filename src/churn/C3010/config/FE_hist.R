dcfd693ef66cca31ef1e10bf038da074b67cd3d4
missvico <vickydiliscia@gmail.com>
2025-12-02 02:50:58 +0000

    comento la subida a kaggle

--- src/churn/C3010/config/FE_hist.R
+++ src/churn/C3010/config/FE_hist.R
@@ -12,7 +12,7 @@ FE_hist$Lags$Run <- TRUE
 FE_hist$Lags$Order <- c(1,2,3,6,12)
 
 FE_hist$DeltaLags$Run <- TRUE
-FE_hist$DeltaLags$Order <- c(1,2,3)
+FE_hist$DeltaLags$Order <- c(1,2,3,6,12)
 FE_hist$spechistoricas <- paste0(util$churn, "/", util$scriptfolder, "/feature_specs/spec_historicas.txt")
 
 FE_hist$specsipc <- paste0(util$churn, "/", util$scriptfolder, "/feature_specs/ipc.txt")
