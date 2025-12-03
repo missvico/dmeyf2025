682da40e7008571e0257b9aba4694164a1e2ef7d
missvico <vickydiliscia@gmail.com>
2025-12-03 17:22:17 +0000

    setup 3002 con segunda semilla

---
+++ src/churn/C3002/config_250061/FE_hist.R
@@ -0,0 +1,28 @@
+util <- list()
+
+util$user <- "vickydiliscia"
+util$scriptfolder <- "C3002"
+util$base <- paste0("/home/", paths$user, "/dmeyf2025")
+util$churn <- paste0(paths$base, "/src/churn")
+
+FE_hist<- list()
+
+FE_hist$Ipc$Run <- TRUE
+FE_hist$Lags$Run <- TRUE
+FE_hist$Lags$Order <- c(1,2)
+
+FE_hist$DeltaLags$Run <- TRUE
+FE_hist$DeltaLags$Order <- c(1,2)
+FE_hist$spechistoricas <- paste0(util$churn, "/", util$scriptfolder, "/feature_specs/spec_historicas.txt")
+
+FE_hist$specsipc <- paste0(util$churn, "/", util$scriptfolder, "/feature_specs/ipc.txt")
+
+# parametros de Feature Engineering Historico de Tendencias
+FE_hist$Tendencias$run <- TRUE
+FE_hist$Tendencias$ventana <- 6
+FE_hist$Tendencias$tendencia <- TRUE
+FE_hist$Tendencias$minimo <- FALSE
+FE_hist$Tendencias$maximo <- FALSE
+FE_hist$Tendencias$promedio <- FALSE
+FE_hist$Tendencias$ratioavg <- FALSE
+FE_hist$Tendencias$ratiomax <- FALSE
